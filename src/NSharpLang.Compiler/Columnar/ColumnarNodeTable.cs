21b0fd67da0ad74d3e6cfa61a95e6167e814c2a8
Spencer Schneidenbach <spencer@avironsoftware.com>
2026-06-24 11:18:28 -0500

    Delete dead columnar node accessors

--- src/NSharpLang.Compiler/Columnar/ColumnarNodeTable.cs
+++ src/NSharpLang.Compiler/Columnar/ColumnarNodeTable.cs
@@ -8,7 +8,6 @@ internal readonly struct ColumnarNodeTable
     private readonly int[] _childStarts;
     private readonly int[] _childCounts;
     private readonly int[] _childIndices;
-    private readonly int[]? _spanStarts;
 
     internal ColumnarNodeTable(
         int[] kinds,
@@ -25,14 +24,11 @@ internal readonly struct ColumnarNodeTable
         _childStarts = childStarts;
         _childCounts = childCounts;
         _childIndices = childIndices;
-        _spanStarts = spanStarts;
     }
 
     internal int Kind(int index) => _kinds[index];
 
     internal int ValueStart(int index) => _valueStarts[index];
-
-    internal int ValueLength(int index) => _valueLengths[index];
 
     internal int[] Kinds => _kinds;
 
@@ -49,11 +45,6 @@ internal readonly struct ColumnarNodeTable
     internal int ChildCount(int index) => _childCounts[index];
 
     internal int Child(int index, int childOrdinal) => _childIndices[_childStarts[index] + childOrdinal];
-
-    internal int SpanStart(int index)
-        => _spanStarts is not null
-            ? _spanStarts[index]
-            : throw new System.InvalidOperationException("Columnar node span starts were not provided.");
 
     internal string Text(string source, int index) => source.Substring(_valueStarts[index], _valueLengths[index]);
 }
